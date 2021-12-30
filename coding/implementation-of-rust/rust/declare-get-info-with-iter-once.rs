0c653aba8ff83ff0324718ad754bc3e6a0a4a70a
Sprise <phanthanhhuy1996@gmail.com>
2021-12-30 11:09:31 +0700

    Update declare-get-info-with-iter-once.rs

--- coding/implementation-of-rust/rust/declare-get-info-with-iter-once.rs
+++ coding/implementation-of-rust/rust/declare-get-info-with-iter-once.rs
@@ -1,3 +1,13 @@
+pub trait UpgradeInfo {
+    /// Opaque type representing a negotiable protocol.
+    type Info: ProtocolName + Clone;
+    /// Iterator returned by `protocol_info`.
+    type InfoIter: IntoIterator<Item = Self::Info>;
+
+    /// Returns the list of protocols that are supported. Used during the negotiation process.
+    fn protocol_info(&self) -> Self::InfoIter;
+}
+
 impl UpgradeInfo for DeflateConfig {
     type Info = &'static [u8];
     type InfoIter = iter::Once<Self::Info>;
